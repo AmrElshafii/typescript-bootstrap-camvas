9d2797315ec1d590b4d3329741a2d9529e9458c6
AmrElshafii <amrabdalaala@gmail.com>
2024-06-27 09:48:13 +0300

    the end

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -378,3 +378,95 @@
 
 // weak 4
 // assign 1
+/*
+// Do Not Edit
+type numandstring = number | string;
+
+abstract class Game {
+    constructor(public title: string, public price: numandstring) {}
+    abstract getLocation() : string;
+    abstract getDiscount() : string;
+}
+
+// Start Edit And Fix
+class RPG extends Game {
+    constructor(title: string, public price: numandstring, public rate: number) {
+        super(title, price)
+    }
+    getLocation() : string {
+        return `Location`
+    }
+    getDiscount() : string {
+        return `Discount`
+    }
+}
+
+class Action extends Game {
+    constructor(title: string, public price: numandstring, public rate: number, public company : string) {
+        super(title, price)
+        this.rate = rate
+        this.company = company
+    }
+    getLocation() : string {
+        return `Location`
+    }
+    getDiscount() : string {
+        return `Discount`
+    }
+}
+// End Edit And Fix
+
+// Do Not Edit
+let gameOne = new RPG("Ys", 100, 10);
+let gameTwo = new Action("Uncharted", 90, 15, "Sony");
+
+console.log(gameOne.title); // "Ys"
+console.log(gameOne.price); // 100
+console.log(gameOne.rate); // 10
+console.log(gameOne.getDiscount()); // "Discount"
+console.log(gameOne.getLocation()); // "Location"
+
+console.log(gameTwo.title); // "Uncharted"
+console.log(gameTwo.price); // 90
+console.log(gameTwo.rate); // 15
+console.log(gameTwo.company); // "Sony"
+console.log(gameTwo.getDiscount()); // "Discount"
+console.log(gameTwo.getLocation()); // "Location"
+*/
+
+// asiign 2
+/*
+// Write Function Code Here
+function showTypes<T1 = undefined, T2 = undefined, T3 = undefined>(input1? : T1, input2? : T2, input3? : T3) {
+    return `${input1 ? input1 : 'Nothing'} - ${input2 ? input2 : 'Nothing'} - ${input3 ? input3 : 'Nothing'}`
+}
+// Do Not Edit Here
+console.log(showTypes()); // Nothing - Nothing - Nothing
+console.log(showTypes<string>("String")); // String - Nothing - Nothing
+console.log(showTypes<string, number>("String", 100)); // String - 100 - Nothing
+console.log(showTypes<string, number, boolean>("String", 100, true)); // String - 100 - true
+*/
+
+// Write Class Code Here
+/*
+class Game<T> {
+    constructor(public title : T, public price : number) {
+        this.title = title
+        this.price = price
+    }
+    getDiscount(discount) {
+        console.log(`The Discount Is ${discount}`)
+    }
+} 
+// Do Not Edit Here
+let gameOne = new Game<string>("Ys", 100);
+let gameTwo = new Game<number>(2064, 100); // There's A Game Called "2064"
+
+console.log(gameOne.title); // "Ys"
+console.log(gameOne.price); // 100
+gameOne.getDiscount("50"); // "The Discount Is 50"
+
+console.log(gameTwo.title); // 2064
+console.log(gameTwo.price); // 100
+gameTwo.getDiscount(80); // "The Discount Is 80"
+*/
