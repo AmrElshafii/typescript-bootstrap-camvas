07c1fd20fa477388d3885c699184fcf7c8bdb16a
AmrElshafii <amrabdalaala@gmail.com>
2024-06-30 16:01:17 +0300

    canvas

--- canvas/index.js
+++ canvas/index.js
@@ -1,21 +1,56 @@
-let myCanvas = document.querySelector('#myCanvas');
-let mycontext = myCanvas.getContext('2d');
-let img = document.querySelector('#my-img');
-img.style.display = 'none';
+// let myCanvas = document.querySelector('#myCanvas');
+// let mycontext = myCanvas.getContext('2d');
+// let img = document.querySelector('#my-img');
+// img.style.display = 'none';
 
 
-let mygrad = mycontext.createLinearGradient(0, 0, myCanvas.width, 0);
-mygrad.addColorStop(0, 'red');
-mygrad.addColorStop(.33, 'blue');
-mygrad.addColorStop(.66, 'red');
-mygrad.addColorStop(1, 'blue');
+// let h = myCanvas.height
+// let w = myCanvas.width
+// mycontext.fillStyle = '#eee'
+// mycontext.fillRect(0, 0, w, h)
+/*
+function colDraw(colNum) {
+    for(let i = 0; i < colNum; i++){
+        let randHeight = Math.round(Math.random() * 150 + 20)
+        mycontext.fillStyle = '#00F'
+        mycontext.fillRect(50 * (i + 1) + (80 * i), randHeight, 70, h - randHeight)
+        mycontext.font = '48px KulminoituvaRegular'
+        mycontext.fillStyle = '#FFF'
+        mycontext.fillText(`${i + 1}`, 50 * (i + 1) + (80 * i) + 25, h - 30)
+    }
+}
+colDraw(10)
 
+mycontext.lineWidth = 5
+mycontext.beginPath()
+mycontext.moveTo(50, 50)
+myCanvas.addEventListener('click', function(e) {
+    mycontext.lineWidth = Number(document.querySelector('#int-2').value)
+    mycontext.strokeStyle = document.querySelector('#int').value
+    mycontext.lineTo(e.clientX, e.clientY)
+    mycontext.stroke()
+})
 
+let fullCircle = 2 * Math.PI
+mycontext.lineWidth = 5
+mycontext.strokeStyle = 'red'
+mycontext.shadowColor = 'red'
+mycontext.shadowOffsetX = '20'
+mycontext.arc(w / 2, h / 2, (h / 2) - 50, 0, fullCircle, false)
+mycontext.stroke()
 
-mycontext.font = '32px system-ui'
-mycontext.strokeStyle = mygrad
-mycontext.strokeText('hello', 10, 32)
+mycontext.lineWidth = 5
+mycontext.strokeStyle = 'red'
+mycontext.shadowColor = 'red'
+mycontext.shadowOffsetX = '-20'
+mycontext.arc(w / 2, h / 2, (h / 2) - 50, 0, fullCircle, false)
+mycontext.stroke()
 
-mycontext.font = '32px system-ui'
-mycontext.fillStyle = '#black'
-mycontext.fillText('hello', 10, 32)
+*/
+
+function hash(str) {
+    let strascies = str.split('').map(letter => letter.charCodeAt(0))
+    return String(strascies.reduce((acc, cur) => acc * cur)).split('').map((num, i, array) => String.fromCharCode(Number(`${num + array[i]}`))).join()
+}
+
+console.log(hash('ahmed'))
